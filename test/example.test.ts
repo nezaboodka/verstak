9504ef0e78e08c33303a43c885529c545499f541
Yury Chetyrko <ychetyrko@gmail.com>
2020-08-02 11:20:31 +0300

    License

--- test/example.test.ts
+++ test/example.test.ts
@@ -1,7 +1,7 @@
 ﻿// The below copyright notice and the license permission notice
 // shall be included in all copies or substantial portions.
 // Copyright (C) 2019-2020 Yury Chetyrko <ychetyrko@gmail.com>
-// License: https://raw.githubusercontent.com/reactronic-front/reactronic/master/LICENSE
+// MIT License: https://raw.githubusercontent.com/nezaboodka/reactronic-front/master/LICENSE
 
 import test from 'ava'
 
