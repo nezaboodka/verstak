4a73dfbbef2c0051902eb16c84d79ee6dc61e49e
Yury Chetyrko <ychetyrko@gmail.com>
2020-10-19 09:08:47 +0300

    Licensing

--- source/api.ts
+++ source/api.ts
@@ -2,6 +2,8 @@
 // shall be included in all copies or substantial portions.
 // Copyright (C) 2019-2020 Yury Chetyrko <ychetyrko@gmail.com>
 // MIT License: https://raw.githubusercontent.com/nezaboodka/reactronic-front/master/LICENSE
+// By contributing, you agree that your contributions will be
+// automatically licensed under the license referred above.
 
 export * from './System'
 export * from './Restyler'
