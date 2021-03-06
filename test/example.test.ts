c83751f6ecd480473f713f11899da5f0b644fc58
Yury Chetyrko <ychetyrko@gmail.com>
2021-03-06 12:24:20 +0300

    License

--- test/example.test.ts
+++ test/example.test.ts
@@ -1,6 +1,6 @@
 ﻿// The below copyright notice and the license permission notice
 // shall be included in all copies or substantial portions.
-// Copyright (C) 2019-2020 Yury Chetyrko <ychetyrko@gmail.com>
+// Copyright (C) 2019-2021 Yury Chetyrko <ychetyrko@gmail.com>
 // MIT License: https://raw.githubusercontent.com/nezaboodka/reactronic-front/master/LICENSE
 // By contributing, you agree that your contributions will be
 // automatically licensed under the license referred above.
