6d3634c9669997106f9dc74663b91ae25acc1657
Yury Chetyrko <ychetyrko@gmail.com>
2021-11-17 08:46:03 +0300

    Cosmetics

--- source/core/Elements.ts
+++ source/core/Elements.ts
@@ -13,8 +13,8 @@ export function RxFragment<E = unknown, O = void>(id: string, args: any, render:
 }
 
 export function RxFragmentWithoutOrdering<E = unknown, O = void>(id: string, args: any, render: Render<E, O>): NodeInfo<E, O> {
-  return RxDom.emit(id, args, render, undefined, RTTI_RX_UNORDERED_FRAGMENT)
+  return RxDom.emit(id, args, render, undefined, RTTI_RX_FRAGMENT_WITHOUT_ORDERING)
 }
 
 const RTTI_RX_FRAGMENT: Rtti<any, any> = { name: 'RxFragment', unordered: false }
-const RTTI_RX_UNORDERED_FRAGMENT: Rtti<any, any> = { name: 'RxSortingFragment', unordered: true }
+const RTTI_RX_FRAGMENT_WITHOUT_ORDERING: Rtti<any, any> = { name: 'RxFragmentWithoutOrdering', unordered: true }
