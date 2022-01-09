c874a35557347294a0490a6e24cdc3772f53fd24
Yury Chetyrko <ychetyrko@gmail.com>
2022-01-09 22:46:13 +0300

    WIP: Get rid of Customize in favor of SuperRender

--- source/core/RxDom.Types.ts
+++ source/core/RxDom.Types.ts
@@ -7,9 +7,9 @@
 
 export type Render<E = unknown, O = void> = (element: E, options: O) => void | Promise<void>
 export type SuperRender<O = unknown, E = void> = ((render: (options: O) => void, element: E) => void)
-export type AsyncSuperRender<O = unknown, E = void> = ((render: (options: O) => Promise<void>, element: E) => Promise<void>)
+// export type AsyncSuperRender<O = unknown, E = void> = ((render: (options: O) => Promise<void>, element: E) => Promise<void>)
 export type Customize<E = unknown, O = void> = (element: E, options: O) => void
-export type AsyncCustomize<E = unknown, O = void> = (element: E, options: O) => Promise<void>
+// export type AsyncCustomize<E = unknown, O = void> = (element: E, options: O) => Promise<void>
 export const enum RxPriority { SyncP0 = 0, AsyncP1 = 1, AsyncP2 = 2 }
 
 export interface RxNodeFactory<E = unknown> {
