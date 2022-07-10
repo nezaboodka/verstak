d3a1cbe249de396ef8e2d1dd1d35df7aecd4cdcc
Yury Chetyrko <ychetyrko@gmail.com>
2022-07-10 16:16:50 +0300

    Basic MergeList tests

--- test/MergeList.test.ts
+++ test/MergeList.test.ts
@@ -6,33 +6,40 @@
 // automatically licensed under the license referred above.
 
 import test from 'ava'
-import { MergeList } from '../source/core/MergeList'
+import { MergeList, MergeListItem } from '../source/core/MergeList'
 
 test('MergeList', t => {
+  // Basic
   const ml = new MergeList<string>(s => s, true)
   const etalon1 = ['Hello', 'Welcome', 'Bye']
   for (const x of etalon1)
     ml.add(x)
-  let i = 0
-  for (const item of ml.items())
-    t.is(item.self, etalon1[i++])
+  t.true(compare(ml.items(), etalon1))
 
-  const etalon2 = ['Added1', 'Hello', 'Bye', 'Added2']
+  // Merge
+  const etalon2 = ['Added1', 'Hello', 'Added2', 'Bye', 'Added3']
   ml.beginMerge()
   for (const x of etalon2)
     if (!ml.tryMerge(x))
       ml.add(x)
   ml.endMerge(true)
-  t.is(ml.count, 4)
+  t.is(ml.count, 5)
   t.is(ml.removedItemCount, 1)
-  t.is(ml.addedItemCount, 2)
-  for (const item of ml.removedItems())
-    t.is(item.self, 'Welcome')
-  i = 1
-  for (const item of ml.addedItems()) {
-    t.is(item.self, `Added${i++}`)
-  }
-  i = 0
-  for (const item of ml.items())
-    t.is(item.self, etalon2[i++])
+  t.is(ml.addedItemCount, 3)
+  t.true(compare(ml.removedItems(), ['Welcome']))
+  t.true(compare(ml.addedItems(), ['Added1', 'Added2', 'Added3']))
+  t.true(compare(ml.items(), etalon2))
 })
+
+function compare(list: Generator<MergeListItem<string>>, array: Array<string>): boolean {
+  let result = true
+  let i = 0
+  for (const item of list) {
+    if (item.self !== array[i]) {
+      result = false
+      break
+    }
+    i++
+  }
+  return result && i === array.length
+}
