8426e8f42ebb600481fad1f37c984467c0d6fca4
Yury Chetyrko <ychetyrko@gmail.com>
2022-10-29 16:58:30 +0300

    WIP: Layout

--- source/core/Layout.ts
+++ source/core/Layout.ts
@@ -5,9 +5,57 @@
 // By contributing, you agree that your contributions will be
 // automatically licensed under the license referred above.
 
-export class LayoutCursor {
-  currentColumn: number = 0
-  currentRow: number = 0
+export class LayoutManager {
+  maxWidth?: number = undefined
+  maxHeight?: number = undefined
+  actualWidth: number = 0
+  actualHeight: number = 0
+  prevColumn: number = 0
+  prevRow: number = 0
+  nextRow: number = 0
+
+  claim(lr: LayoutRequest, result: LayoutArea): LayoutArea {
+    if (!lr.area) {
+      if (lr.fromNewLine && this.prevColumn > 0) {
+        this.prevColumn = 0
+        this.prevRow = this.nextRow
+      }
+      // Horizontal
+      let w = lr.width ?? 1
+      if (w === Infinity)
+        w = this.maxWidth ?? this.actualWidth
+      if (w >= 0) {
+        result.x1 = this.prevColumn + 1
+        result.x2 = result.x1 + w
+        if (lr.cursorRight !== false)
+          this.prevColumn += w
+      }
+      else {
+        result.x1 = this.prevColumn + w
+        result.x2 = this.prevColumn
+      }
+      // Vertical
+      let h = lr.height ?? 1
+      if (h === Infinity)
+        h = this.maxHeight ?? this.actualHeight
+      if (h >= 0) {
+        result.y1 = this.prevRow + 1
+        result.y2 = result.y1 + h
+        if (lr.cursorDown !== false) {
+          const n = this.prevRow + h
+          if (n > this.nextRow)
+            this.nextRow = this.actualHeight = n
+        }
+      }
+      else {
+        result.y1 = this.prevRow + h
+        result.y2 = this.prevRow
+      }
+    }
+    else
+      LayoutAreaUtils.parseLayoutArea(lr.area, result)
+    return result
+  }
 }
 
 export interface LayoutRequest {
@@ -16,6 +64,7 @@ export interface LayoutRequest {
   area?: string
   cursorRight?: boolean
   cursorDown?: boolean
+  fromNewLine?: boolean
 }
 
 export interface LayoutArea {
--- source/html/Block.ts
+++ source/html/Block.ts
@@ -6,21 +6,34 @@
 // automatically licensed under the license referred above.
 
 import { LoggingOptions, Monitor } from 'reactronic'
-import { VerstakNode, Render, Priority, LayoutRequest } from '../core/api'
+import { VerstakNode, Render, Priority, LayoutRequest, Reaction, Inline } from '../core/api'
 import { Div, RxDiv } from './HtmlElements'
+
+export function Block<M = unknown, R = void>(name: string,
+  layout: LayoutRequest | null, renderer: Render<HTMLDivElement, M, R>,
+  priority?: Priority): VerstakNode<HTMLDivElement, M, R> {
+  return Div(name, renderer, priority)
+}
 
 export function RxBlock<M = unknown, R = void>(name: string,
   layout: LayoutRequest | null, triggers: unknown,
-  renderer: Render<HTMLDivElement, M, R>, priority?: Priority,
-  monitor?: Monitor, throttling?: number, logging?: Partial<LoggingOptions>):
-  VerstakNode<HTMLDivElement, M, R> {
-  return RxDiv(name, triggers, renderer, priority, monitor, throttling,
-    logging)
+  renderer: Render<HTMLDivElement, M, R>,
+  priority?: Priority, monitor?: Monitor, throttling?: number,
+  logging?: Partial<LoggingOptions>): VerstakNode<HTMLDivElement, M, R> {
+  return RxDiv(name, triggers, renderer,
+    priority, monitor, throttling, logging)
 }
 
-export function Block<M = unknown, R = void>(name: string,
-  layout: LayoutRequest | null,
+export function Cluster<M = unknown, R = void>(name: string,
   renderer: Render<HTMLDivElement, M, R>, priority?: Priority):
   VerstakNode<HTMLDivElement, M, R> {
-  return Div(name, renderer, priority)
+  return Inline(name, renderer, priority)
+}
+
+export function RxCluster<M = unknown, R = void>(name: string,
+  triggers: unknown, renderer: Render<HTMLDivElement, M, R>,
+  priority?: Priority, monitor?: Monitor, throttling?: number,
+  logging?: Partial<LoggingOptions>): VerstakNode<HTMLDivElement, M, R> {
+  return Reaction(name, triggers, renderer,
+    priority, monitor, throttling, logging)
 }
