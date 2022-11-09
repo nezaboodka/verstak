b9c8f61c47466214c40c02c20c2502bfbad38945
Yury Chetyrko <ychetyrko@gmail.com>
2022-11-09 08:32:22 +0300

    Cosmetics

--- source/html/Blocks.ts
+++ source/html/Blocks.ts
@@ -36,7 +36,7 @@ export function Block<M = unknown, R = void>(name: string,
 
 // Text (either plain or formatted in form of markdown)
 
-export function $(strings: TemplateStringsArray, ...args: any[]): VBlock<HTMLElement, void, void> {
+export function $text(strings: TemplateStringsArray, ...args: any[]): VBlock<HTMLElement, void, void> {
   const content = String.raw(strings, ...args)
   return VBlock.claim("", { render(e) { e.innerText = content } }, VerstakTags.text)
 }
