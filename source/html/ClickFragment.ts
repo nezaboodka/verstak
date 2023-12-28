4922249a2bc8924f92de9cc3e545142c8a4b1876
Vadim Seledets <vadim.seledets@nezaboodka.com>
2023-12-28 12:27:20 +0300

    Introduce "ClickFragment"

---
+++ source/html/ClickFragment.ts
@@ -0,0 +1,27 @@
+// The below copyright notice and the license permission notice
+// shall be included in all copies or substantial portions.
+// Copyright (C) 2019-2024 Nezaboodka Software <contact@nezaboodka.com>
+// License: https://raw.githubusercontent.com/nezaboodka/verstak/master/LICENSE
+// By contributing, you agree that your contributions will be
+// automatically licensed under the license referred above.
+
+import { Mode } from "reactronic"
+import { CustomFragment } from "./Elements.js"
+
+export function ClickFragment(target: HTMLElement, action: (() => void) | undefined, key?: string): void {
+
+  if (action) {
+    CustomFragment({
+      key,
+      mode: Mode.IndependentUpdate,
+      triggers: { target },
+      update() {
+        const pointer = target.sensors.pointer
+        if (pointer.clicked) {
+          action()
+        }
+      },
+    })
+  }
+
+}
