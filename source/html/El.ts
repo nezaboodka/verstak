f0af911d065625f1f4193d6ef6d851eaa07101af
Yury Chetyrko <ychetyrko@gmail.com>
2024-02-17 20:09:15 +0300

    Code structure

--- source/html/El.ts
+++ source/html/El.ts
@@ -150,7 +150,7 @@ export class ElImpl<T extends Element = any, M = any> implements El<T, M> {
   set kind(value: ElKind) {
     if (value !== this._kind || this.node.stamp >= Number.MAX_SAFE_INTEGER - 1) {
       if (this.native !== undefined)
-        Apply.kind(this, value)
+        ElImpl.applyKind(this, value)
       this._kind = value
     }
   }
@@ -171,7 +171,7 @@ export class ElImpl<T extends Element = any, M = any> implements El<T, M> {
         prevElLayoutInfo, layoutInfo)
       if (!equalElCoords(coords, this._coords)) {
         if (this.native !== undefined)
-          Apply.coords(this, coords)
+          ElImpl.applyCoords(this, coords)
         this._coords = coords
       }
       this._area = value ?? { }
@@ -185,11 +185,11 @@ export class ElImpl<T extends Element = any, M = any> implements El<T, M> {
     const w = this._width
     let updated = false
     if (value.min !== w.min) {
-      Apply.minWidth(this, value.min ?? "")
+      ElImpl.applyMinWidth(this, value.min ?? "")
       updated = true
     }
     if (value.max !== w.max) {
-      Apply.maxWidth(this, value.max ?? "")
+      ElImpl.applyMaxWidth(this, value.max ?? "")
       updated = true
     }
     if (updated)
@@ -201,11 +201,11 @@ export class ElImpl<T extends Element = any, M = any> implements El<T, M> {
     const w = this._height
     let updated = false
     if (value.min !== w.min) {
-      Apply.minHeight(this, value.min ?? "")
+      ElImpl.applyMinHeight(this, value.min ?? "")
       updated = true
     }
     if (value.max !== w.max) {
-      Apply.maxHeight(this, value.max ?? "")
+      ElImpl.applyMaxHeight(this, value.max ?? "")
       updated = true
     }
     if (updated)
@@ -215,7 +215,7 @@ export class ElImpl<T extends Element = any, M = any> implements El<T, M> {
   get alignment(): Align { return this._alignment }
   set alignment(value: Align) {
     if (value !== this._alignment) {
-      Apply.alignment(this, value, this._extraAlignment, this._stretchingStrengthX, this._stretchingStrengthY)
+      ElImpl.applyAlignment(this, value, this._extraAlignment, this._stretchingStrengthX, this._stretchingStrengthY)
       this._alignment = value
     }
   }
@@ -223,7 +223,7 @@ export class ElImpl<T extends Element = any, M = any> implements El<T, M> {
   get extraAlignment(): Align { return this._extraAlignment }
   set extraAlignment(value: Align) {
     if (value !== this._extraAlignment) {
-      Apply.alignment(this, this._alignment, value, this._stretchingStrengthX, this._stretchingStrengthY)
+      ElImpl.applyAlignment(this, this._alignment, value, this._stretchingStrengthX, this._stretchingStrengthY)
       this._extraAlignment = value
     }
   }
@@ -231,7 +231,7 @@ export class ElImpl<T extends Element = any, M = any> implements El<T, M> {
   get stretchingStrengthX(): number | undefined { return this._stretchingStrengthX }
   set stretchingStrengthX(value: number | undefined) {
     if (value !== this._stretchingStrengthX) {
-      Apply.stretchingStrengthX(this, value ?? 0)
+      ElImpl.applyStretchingStrengthX(this, value ?? 0)
       this._stretchingStrengthX = value
     }
   }
@@ -239,7 +239,7 @@ export class ElImpl<T extends Element = any, M = any> implements El<T, M> {
   get stretchingStrengthY(): number | undefined { return this._stretchingStrengthY }
   set stretchingStrengthY(value: number | undefined) {
     if (value !== this._stretchingStrengthY) {
-      Apply.stretchingStrengthY(this, value ?? 0)
+      ElImpl.applyStretchingStrengthY(this, value ?? 0)
       this._stretchingStrengthY = value
     }
   }
@@ -247,7 +247,7 @@ export class ElImpl<T extends Element = any, M = any> implements El<T, M> {
   get contentWrapping(): boolean { return this._contentWrapping }
   set contentWrapping(value: boolean) {
     if (value !== this._contentWrapping) {
-      Apply.contentWrapping(this, value)
+      ElImpl.applyContentWrapping(this, value)
       this._contentWrapping = value
     }
   }
@@ -255,7 +255,7 @@ export class ElImpl<T extends Element = any, M = any> implements El<T, M> {
   get overlayVisible(): boolean | undefined { return this._overlayVisible }
   set overlayVisible(value: boolean | undefined) {
     if (value !== this._overlayVisible) {
-      Apply.overlayVisible(this, value)
+      ElImpl.applyOverlayVisible(this, value)
       this._overlayVisible = value
     }
   }
@@ -263,7 +263,7 @@ export class ElImpl<T extends Element = any, M = any> implements El<T, M> {
   get style(): CSSStyleDeclaration { return (this.native as any).style }
 
   useStylingPreset(stylingPresetName: string, enabled?: boolean): void {
-    Apply.stylingPreset(this, this._hasStylingPresets, stylingPresetName, enabled)
+    ElImpl.applyStylingPreset(this, this._hasStylingPresets, stylingPresetName, enabled)
     this._hasStylingPresets = true
   }
 
@@ -274,6 +274,175 @@ export class ElImpl<T extends Element = any, M = any> implements El<T, M> {
     const layoutInfo = this.layoutInfo = new ElLayoutInfo(prevElLayoutInfo)
     layoutInfo.x = 1
     layoutInfo.y = layoutInfo.runningMaxY + 1
+  }
+
+  private static applyKind<T extends Element>(element: ElImpl<T, any>, value: ElKind): void {
+    const kind = Constants.layouts[value]
+    kind && element.native.setAttribute(Constants.kindAttrName, kind)
+    VerstakDriversByLayout[value](element as any)
+  }
+
+  private static applyCoords<T extends Element>(element: ElImpl<T, any>, value: ElCoords | undefined): void {
+    const s = element.style
+    if (value) {
+      const x1 = value.x1 || 1
+      const y1 = value.y1 || 1
+      const x2 = value.x2 || x1
+      const y2 = value.y2 || y1
+      s.gridArea = `${y1} / ${x1} / span ${y2 - y1 + 1} / span ${x2 - x1 + 1}`
+    }
+    else
+      s.gridArea = ""
+  }
+
+  private static applyMinWidth<T extends Element>(element: ElImpl<T, any>, value: string): void {
+    element.style.minWidth = `${value}`
+  }
+
+  private static applyMaxWidth<T extends Element>(element: ElImpl<T, any>, value: string): void {
+    element.style.maxWidth = `${value}`
+  }
+
+  private static applyMinHeight<T extends Element>(element: ElImpl<T, any>, value: string): void {
+    element.style.minHeight = `${value}`
+  }
+
+  private static applyMaxHeight<T extends Element>(element: ElImpl<T, any>, value: string): void {
+    element.style.maxHeight = `${value}`
+  }
+
+  private static applyAlignment<T extends Element>(element: ElImpl<T, any>,
+    primary: Align, extra: Align,
+    strengthX: number | undefined,
+    strengthY: number | undefined): void {
+    const s = element.style
+    // Primary
+    let v1 = ""; let h1 = ""; let t1 = ""
+    if (primary !== Align.default) { // if not auto mode
+      v1 = AlignToCss[(primary >> 3) & 0b11]
+      h1 = AlignToCss[primary & 0b11]
+      t1 = TextAlignCss[primary & 0b11]
+    }
+    s.alignSelf = v1
+    s.justifySelf = h1
+    if ((primary & Align.left) !== 0 && strengthX === undefined)
+      ElImpl.applyStretchingStrengthX(element, 1)
+    if ((primary & Align.top) !== 0 && strengthY === undefined)
+      ElImpl.applyStretchingStrengthY(element, 1)
+    // Extra
+    let v2 = ""; let h2 = ""; let t2 = ""
+    if (extra !== Align.default) { // if not auto mode
+      v2 = AlignToCss[(extra >> 3) & 0b11]
+      h2 = AlignToCss[extra & 0b11]
+      t2 = TextAlignCss[extra & 0b11]
+    }
+    else {
+      v2 = v1
+      h2 = h1
+      t2 = t1
+    }
+    // Primary
+    s.justifyContent = v2
+    s.alignItems = h2
+    s.textAlign = t2
+  }
+
+  private static applyStretchingStrengthX<T extends Element>(element: ElImpl<T, any>, value: number): void {
+    const bNode = element.node
+    const driver = bNode.driver
+    if (driver.isPartition) {
+      const s = element.style
+      s.alignSelf = value > 0 ? "stretch" : ""
+    }
+    else {
+      const s = element.style
+      if (value > 0) {
+        s.flexGrow = `${value}`
+        s.flexBasis = "0"
+      }
+      else {
+        s.flexGrow = ""
+        s.flexBasis = ""
+      }
+      const hostDriver = bNode.host.driver
+      if (hostDriver.isPartition) {
+        const host = bNode.host.seat!.instance as RxNode<El<T, any>>
+        ElImpl.applyStretchingStrengthX(host.element as ElImpl, 1)
+      }
+    }
+  }
+
+  private static applyStretchingStrengthY<T extends Element>(element: ElImpl<T, any>, value: number): void {
+    const bNode = element.node
+    const driver = bNode.driver
+    if (driver.isPartition) {
+      const s = element.style
+      s.flexGrow = value > 0 ? `${value}` : ""
+    }
+    else {
+      const hostDriver = bNode.host.driver
+      if (hostDriver.isPartition) {
+        const host = bNode.host.seat!.instance as RxNode<El<T, any>>
+        ElImpl.applyAlignment(host.element as ElImpl, Align.stretchHeight, Align.default, undefined, undefined)
+        ElImpl.applyStretchingStrengthY(host.element as ElImpl, value)
+      }
+    }
+  }
+
+  private static applyContentWrapping<T extends Element>(element: ElImpl<T, any>, value: boolean): void {
+    const s = element.style
+    if (value) {
+      s.flexFlow = "wrap"
+      s.overflow = ""
+      s.textOverflow = ""
+      s.whiteSpace = ""
+    }
+    else {
+      s.flexFlow = ""
+      s.overflow = "hidden"
+      s.textOverflow = "ellipsis"
+      s.whiteSpace = "nowrap"
+    }
+  }
+
+  private static applyOverlayVisible<T extends Element>(element: ElImpl<T, any>, value: boolean | undefined): void {
+    const s = element.style
+    const host = RxNode.findMatchingHost<El, El>(element.node, n =>
+      n.element.native instanceof HTMLElement || n.element.native instanceof SVGElement)
+    const nativeHost = host?.element.native
+    if (value === true) {
+      const doc = document.body
+      const rect = nativeHost.getBoundingClientRect()
+      if (doc.offsetWidth - rect.left > rect.right) // rightward
+        s.left = "0", s.right = ""
+      else // leftward
+        s.left = "", s.right = "0"
+      if (doc.clientHeight - rect.top > rect.bottom) // downward
+        s.top = "100%", s.bottom = ""
+      else // upward
+        s.top = "", s.bottom = "100%"
+      s.display = ""
+      s.position = "absolute"
+      s.minWidth = "100%"
+      s.boxSizing = "border-box"
+      nativeHost.style.position = "relative"
+    }
+    else {
+      nativeHost.style.position = ""
+      if (value === false)
+        s.display = "none"
+      else // overlayVisible === undefined
+        s.position = s.display = s.left = s.right = s.top = s.bottom = "" // clear
+    }
+  }
+
+  private static applyStylingPreset<T extends Element>(element: ElImpl<T, any>, secondary: boolean, styleName: string, enabled?: boolean): void {
+    const native = element.native
+    enabled ??= true
+    if (secondary)
+      native.classList.toggle(styleName, enabled)
+    else
+      native.className = enabled ? styleName : ""
   }
 }
 
@@ -431,192 +600,6 @@ export class CursorCommandDriver extends ElDriver<Element, unknown> {
   constructor() {
     super("cursor", false, el => el.kind = ElKind.cursor)
   }
-}
-
-export class Apply {
-  static kind<T extends Element>(element: ElImpl<T, any>, value: ElKind): void {
-    const kind = Constants.layouts[value]
-    kind && element.native.setAttribute(Constants.kindAttrName, kind)
-    VerstakDriversByLayout[value](element as any)
-  }
-
-  static coords<T extends Element>(element: ElImpl<T, any>, value: ElCoords | undefined): void {
-    const s = element.style
-    if (value) {
-      const x1 = value.x1 || 1
-      const y1 = value.y1 || 1
-      const x2 = value.x2 || x1
-      const y2 = value.y2 || y1
-      s.gridArea = `${y1} / ${x1} / span ${y2 - y1 + 1} / span ${x2 - x1 + 1}`
-    }
-    else
-      s.gridArea = ""
-  }
-
-  static minWidth<T extends Element>(element: ElImpl<T, any>, value: string): void {
-    element.style.minWidth = `${value}`
-  }
-
-  static maxWidth<T extends Element>(element: ElImpl<T, any>, value: string): void {
-    element.style.maxWidth = `${value}`
-  }
-
-  static minHeight<T extends Element>(element: ElImpl<T, any>, value: string): void {
-    element.style.minHeight = `${value}`
-  }
-
-  static maxHeight<T extends Element>(element: ElImpl<T, any>, value: string): void {
-    element.style.maxHeight = `${value}`
-  }
-
-  static alignment<T extends Element>(element: ElImpl<T, any>,
-    primary: Align, extra: Align,
-    strengthX: number | undefined,
-    strengthY: number | undefined): void {
-    const s = element.style
-    // Primary
-    let v1 = ""; let h1 = ""; let t1 = ""
-    if (primary !== Align.default) { // if not auto mode
-      v1 = AlignToCss[(primary >> 3) & 0b11]
-      h1 = AlignToCss[primary & 0b11]
-      t1 = TextAlignCss[primary & 0b11]
-    }
-    s.alignSelf = v1
-    s.justifySelf = h1
-    if ((primary & Align.left) !== 0 && strengthX === undefined)
-      Apply.stretchingStrengthX(element, 1)
-    if ((primary & Align.top) !== 0 && strengthY === undefined)
-      Apply.stretchingStrengthY(element, 1)
-    // Extra
-    let v2 = ""; let h2 = ""; let t2 = ""
-    if (extra !== Align.default) { // if not auto mode
-      v2 = AlignToCss[(extra >> 3) & 0b11]
-      h2 = AlignToCss[extra & 0b11]
-      t2 = TextAlignCss[extra & 0b11]
-    }
-    else {
-      v2 = v1
-      h2 = h1
-      t2 = t1
-    }
-    // Primary
-    s.justifyContent = v2
-    s.alignItems = h2
-    s.textAlign = t2
-  }
-
-  static stretchingStrengthX<T extends Element>(element: ElImpl<T, any>, value: number): void {
-    const bNode = element.node
-    const driver = bNode.driver
-    if (driver.isPartition) {
-      const s = element.style
-      s.alignSelf = value > 0 ? "stretch" : ""
-    }
-    else {
-      const s = element.style
-      if (value > 0) {
-        s.flexGrow = `${value}`
-        s.flexBasis = "0"
-      }
-      else {
-        s.flexGrow = ""
-        s.flexBasis = ""
-      }
-      const hostDriver = bNode.host.driver
-      if (hostDriver.isPartition) {
-        const host = bNode.host.seat!.instance as RxNode<El<T, any>>
-        Apply.stretchingStrengthX(host.element as ElImpl, 1)
-      }
-    }
-  }
-
-  static stretchingStrengthY<T extends Element>(element: ElImpl<T, any>, value: number): void {
-    const bNode = element.node
-    const driver = bNode.driver
-    if (driver.isPartition) {
-      const s = element.style
-      s.flexGrow = value > 0 ? `${value}` : ""
-    }
-    else {
-      const hostDriver = bNode.host.driver
-      if (hostDriver.isPartition) {
-        const host = bNode.host.seat!.instance as RxNode<El<T, any>>
-        Apply.alignment(host.element as ElImpl, Align.stretchHeight, Align.default, undefined, undefined)
-        Apply.stretchingStrengthY(host.element as ElImpl, value)
-      }
-    }
-  }
-
-  static contentWrapping<T extends Element>(element: ElImpl<T, any>, value: boolean): void {
-    const s = element.style
-    if (value) {
-      s.flexFlow = "wrap"
-      s.overflow = ""
-      s.textOverflow = ""
-      s.whiteSpace = ""
-    }
-    else {
-      s.flexFlow = ""
-      s.overflow = "hidden"
-      s.textOverflow = "ellipsis"
-      s.whiteSpace = "nowrap"
-    }
-  }
-
-  static overlayVisible<T extends Element>(element: ElImpl<T, any>, value: boolean | undefined): void {
-    const s = element.style
-    const host = RxNode.findMatchingHost<El, El>(element.node, n =>
-      n.element.native instanceof HTMLElement || n.element.native instanceof SVGElement)
-    const nativeHost = host?.element.native
-    if (value === true) {
-      const doc = document.body
-      const rect = nativeHost.getBoundingClientRect()
-      if (doc.offsetWidth - rect.left > rect.right) // rightward
-        s.left = "0", s.right = ""
-      else // leftward
-        s.left = "", s.right = "0"
-      if (doc.clientHeight - rect.top > rect.bottom) // downward
-        s.top = "100%", s.bottom = ""
-      else // upward
-        s.top = "", s.bottom = "100%"
-      s.display = ""
-      s.position = "absolute"
-      s.minWidth = "100%"
-      s.boxSizing = "border-box"
-      nativeHost.style.position = "relative"
-    }
-    else {
-      nativeHost.style.position = ""
-      if (value === false)
-        s.display = "none"
-      else // overlayVisible === undefined
-        s.position = s.display = s.left = s.right = s.top = s.bottom = "" // clear
-    }
-  }
-
-  static stylingPreset<T extends Element>(element: ElImpl<T, any>, secondary: boolean, styleName: string, enabled?: boolean): void {
-    const native = element.native
-    enabled ??= true
-    if (secondary)
-      native.classList.toggle(styleName, enabled)
-    else
-      native.className = enabled ? styleName : ""
-  }
-
-  // static findEffectiveHtmlElementHost(node: RxNode): RxNode<El<HTMLElement | SVGElement>> {
-  //   let p = node.host
-  //   while (p.seat!.instance.element.native instanceof HTMLElement === false &&
-  //     p.seat!.instance.element.native instanceof SVGElement === false && p !== node)
-  //     p = p.host
-  //   return p.seat!.instance as RxNode<El<HTMLElement | SVGElement>>
-  // }
-
-  // static findPrevSiblingHtmlElement(seat: MergedItem<RxNode>): MergedItem<RxNode<El<HTMLElement | SVGElement>>> | undefined {
-  //   let p = seat.prev
-  //   while (p && !(p.instance.element.native instanceof HTMLElement) && !(p.instance.element.native instanceof SVGElement))
-  //     p = p.prev
-  //   return p
-  // }
 }
 
 // Constants
