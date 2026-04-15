725925b22c5449fc6d89d355407f920a9e024c4d
Yury Chetyrko <ychetyrko@gmail.com>
2026-04-15 20:00:55 +0300

    LiveTime

--- source/components/LiveTime.ts
+++ source/components/LiveTime.ts
@@ -7,7 +7,7 @@
 
 import { RxObject, transaction } from "reactronic"
 
-export class CurrentTime extends RxObject {
+export class LiveTime extends RxObject {
   hour: number = 0
   minute: number = 0
   second: number = 0
